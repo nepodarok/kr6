aa865ac4840071c8b48591e069bd54658ab52b3e
nepodarok <aleksandrnepodarok@gmail.com>
2017-04-01 19:18:42 +0600

    hidden edit profile button from another users

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   root 'home#index'
   devise_for :users
-  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+  # mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
 
 
 
